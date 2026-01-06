b8a9732e04e9744765860779c27e1a6a251c543a
Souvik Kumar Maji <145246361+souvik-04@users.noreply.github.com>
2026-01-06 20:20:20 +0530

    Update CORS configuration to allow all origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173/", credentials: true, }))
+app.use(cors({ origin: "*", credentials: true, }))
 
 app.use(express.json());
 app.use(cookieParser());
