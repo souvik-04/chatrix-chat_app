f73fb0b69440d3e5f016f6a4fa4e4c6504ba1ae2
Souvik Kumar Maji <145246361+souvik-04@users.noreply.github.com>
2026-01-06 19:50:53 +0530

    Simplify CORS configuration in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,10 +16,7 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, 
-  })
+  cors()
 );
 
 app.use(express.json());
