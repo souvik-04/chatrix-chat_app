bdb03b65229c128e3bb7f3eb04891cf6855e5557
Souvik Kumar Maji <souvikmaji592@gmail.com>
2026-01-06 21:05:47 +0530

    cookie clear

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,9 +100,18 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
-  res.status(200).json({ success: true, message: "Logout successful" });
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: true,       // REQUIRED for SameSite=None
+    sameSite: "none",   // 🔥 IMPORTANT
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logout successful",
+  });
 }
+
 
 
 export async function onboard(req, res) {
